c340a50f987cfa63e4ab68f276acd323c5a645af
George Honeywell <george.honeywell@live.co.uk>
2023-09-12 10:17:24 +0100

    Initial Commit.
    Initial project commit.

---
+++ Source/PopOneHundred/PopOneHundredGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PopOneHundredGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POPONEHUNDRED_API APopOneHundredGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
