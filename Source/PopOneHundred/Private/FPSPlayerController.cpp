b644a43b8876b756fde941b644f8afbb7cd48495
George Honeywell <george.honeywell@live.co.uk>
2023-09-27 19:43:03 +0100

    Added aim sight/dot to players viewport

---
+++ Source/PopOneHundred/Private/FPSPlayerController.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSPlayerController.h"
+
+#include "Blueprint/UserWidget.h"
+
+void AFPSPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	m_worldRef = GetWorld();
+	m_fpsCharacter = Cast<AFPSCharacter>(m_worldRef->GetFirstPlayerController()->GetCharacter());
+
+	if(UUserWidget* aimDot = CreateWidget(this, m_aimDot))
+		aimDot->AddToViewport();
+}
---
+++ Source/PopOneHundred/Public/FPSPlayerController.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FPSCharacter.h"
+#include "GameFramework/PlayerController.h"
+#include "FPSPlayerController.generated.h"
+
+UCLASS()
+class POPONEHUNDRED_API AFPSPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+protected:
+	virtual void BeginPlay() override;
+
+	AFPSCharacter* m_fpsCharacter = nullptr;
+
+	UWorld* m_worldRef = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = PlayerHUD)
+		TSubclassOf<class UUserWidget> m_aimDot;
+};
